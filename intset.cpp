ad721aa21619e6e815e7e57bfd616b09bf52ef0f
BFerkol <43259603+BFerkol@users.noreply.github.com>
2020-11-16 16:01:25 -0800

    Add files via upload

---
+++ intset.cpp
@@ -0,0 +1,67 @@
+/* Benjamin Ferkol
+ * CPSC 3220
+*/
+
+#include <iostream>
+#include <assert.h>
+#include <algorithm>
+#include "intset.h"
+
+using namespace std;
+
+Intset::Intset()
+{
+    head = NULL;
+}
+
+Intset::~Intset()
+{
+    while (head != NULL) remove(head->key);
+}
+
+/* Return true if key is in the set */
+bool Intset::find(int key)
+{
+    for (Node *n = head; n != NULL; n = n->next)
+        if (n->key == key) return true;
+    return false;
+}
+
+/* Inserts a new key.  It is an error if key is already in the set. */
+void Intset::insert(int key)
+{
+    assert (!find(key));
+
+    // Insert at beginning of list, swap down list until
+    // in correct location
+    head = new Node(key, head);
+    for (Node *n = head; n->next != NULL && n->next->key < n->key; n = n->next)
+        swap(n->key, n->next->key);
+}
+
+/* Removes a key.  It is an error if key isn't in the set */
+void Intset::remove(int key)
+{
+    assert (find(key));
+
+    // Delete head node?
+    if (head->key == key) {
+        Node *to_delete = head;
+        head = head->next;
+        delete to_delete;
+    } else {
+        // Walk down list and stop one node before the one to
+        // delete
+        Node *n = head;
+        while (n->next->key != key) n = n->next;
+        Node *to_delete = n->next;
+        n->next = to_delete->next;
+        delete to_delete;
+    }
+}
+
+void Intset::print(void)
+{
+    for (Node *n = head; n != NULL; n = n->next)
+        cout << n->key << "\n";
+}
---
+++ intset.h
@@ -0,0 +1,24 @@
+#ifndef INTSET_H /* Prevent multiple inclusion... */
+#define INTSET_H
+
+struct Node {
+  int key;
+  Node *next;
+  Node (int k, Node *n) { key = k; next = n; }
+};
+
+class Intset {
+
+ private: 
+  Node *head;
+
+ public:
+  Intset();
+  ~Intset();
+  bool find(int key);
+  void insert(int key);
+  void remove(int key);
+  void print(void);
+};
+
+#endif
